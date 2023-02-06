b4df9bc64542d801d726e9f00f7ede0a29f7599d
Shunya Yokoi <88517400+shun-1129@users.noreply.github.com>
2023-02-06 23:00:38 +0900

    commit

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
-from wsgiref.util import request_uri
 from fastapi import FastAPI
 from pydantic import BaseModel
+import requests
+import json
 
 app = FastAPI()
 
 class Item(BaseModel):
     name: str
-    age: float
+    price: float
 
 @app.get('/')
 async def root():
@@ -18,6 +19,10 @@ def read_item(item_id: int, q: str = None):
         return {'item_id': item_id, 'q': q}
     return {'item_id': item_id}
 
-@app.post('/items')
+@app.post("/items")
 def update_item(item: Item):
-    return {'item_name': item.name, 'item_age': item.age}
+    return {"item_name": item.name, "twice price": item.price * 2}
+
+@app.post('/json_post')
+def update_item(item: Item):
+    return {"item_name": item.name, "twice price": item.price * 2}
